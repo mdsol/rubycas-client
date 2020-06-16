0ea96936255d624006c71cd3fa6cb82326137d44
wduty <wduty@mdsol.com>
2020-06-16 12:47:29 -0400

    pool 2

--- lib/active_model_redis_store.rb
+++ lib/active_model_redis_store.rb
@@ -11,7 +11,8 @@ module ActionDispatch
     # class ActiveModelRedisStore < ActionDispatch::Session::AbstractStore
       def write_session(env, sid, session_data, options = nil)
         Rails.logger.info("------set_session-------->  #{env.inspect[0..1000]}, sid: #{sid}")
-        Rails.logger.info("------set_session---test get----->  #{@pool.get(sid) rescue 'nope'}")
+        Rails.logger.info("------set_session---test get1----->  #{@pool rescue 'no pool'}")
+        Rails.logger.info("------set_session---test get2----->  #{@pool.get(sid) rescue 'nope'}")
         if session = with { |c| c.get(sid) }
           # Copy session_id and service_ticket into the session_data
           %w(session_id service_ticket).each { |key| session_data[key] = session[key] if session[key] }
